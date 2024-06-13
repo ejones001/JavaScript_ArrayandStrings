3ade4699c85def2d7ac0962a5fcb193c2d634526
ejones001 <ejonesnavy@gmail.com>
2024-06-12 20:31:08 -0500

    Manipulation

---
+++ manip.js
@@ -0,0 +1,7 @@
+// Create a string with some text content
+let message = "Hello, World!";
+
+// Obtain the length of the string using the length property
+let lengthOfMessage = message.length;
+
+console.log('Length of the string:', lengthOfMessage); // Output: 13
